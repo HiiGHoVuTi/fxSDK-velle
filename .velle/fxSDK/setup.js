8768e9d15da2541e88bcfc18e664631d7914eadf
Maxime <emixampons@gmail.com>
2021-08-06 19:15:04 +0200

    added back the command

--- .velle/fxSDK/setup.js
+++ .velle/fxSDK/setup.js
@@ -2,7 +2,7 @@
 const PROJECT_NAME = readLocalConfig("project.name");
 
 const output
-      = true// exec("fxsdk new " + PROJECT_NAME)
+      = exec("fxsdk new " + PROJECT_NAME)
       && appendToFile(".gitignore", ".velle/")
       && appendToFile("README.md", "# " + PROJECT_NAME);
 
